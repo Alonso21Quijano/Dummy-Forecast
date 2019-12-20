2c5e2f39e3c763055b6bf63064909c10c30545f2
Чеканов Михаил <32439309+AlonsoQuixano@users.noreply.github.com>
2019-12-20 22:42:10 +0300

    Update message_manager.py

--- message_manager.py
+++ message_manager.py
@@ -4,12 +4,18 @@ import subprocess
 with open('client_database.json') as json_file:
     db = json.load(json_file)
     for client in db['Clients']:
-        indexes = list(client['Cities_ID'])
-        for db_city in db['Cities']:
-            if db_city['ID'] in indexes:
-                indexes.remove(db_city['ID'])
-                msg_file = db_city['name'] + '.' + db_city['country_code'] + '.msg'
-                subprocess.call(["./messenger.sh", client['phone'], msg_file])
-                
-            if not len(indexes):
-                break
+        try:
+            indexes = list(map(int, client['Cities_ID']))
+            for db_city in db['Cities']:
+                if int(db_city['ID']) in indexes:
+                    indexes.remove(db_city['ID'])
+                    msg_file = db_city['name'] + '.' + db_city['country_code'] + '.msg'
+                    try:
+                        subprocess.call(["./messenger.sh", client['phone'], msg_file])
+                    except Exception as e:
+                        print("failed to send sms to",  client['phone'])
+                    
+                if not len(indexes):
+                    break
+        except Exception as e:
+                        print("problem with client",  client['phone'])
